0db75b69fe07d2a4da27c589be1072a954b16f9f
Bhushan Mendhe <bhushanmendhe15@gmail.com>
2021-09-25 08:53:28 +0530

    javascript completed

--- javascript.js
+++ javascript.js
@@ -1,9 +1,22 @@
 var userDOB = document.querySelector("#user-dob");
 var luckyNumber = document.querySelector("#lucky-number");
 var btnBirthdayCheck = document.querySelector("#btn-birthday-check");
+var outputBox = document.querySelector("#show-message");
 
 btnBirthdayCheck.addEventListener("click",checkBirthdayHandler)
 
 function checkBirthdayHandler() {
-    console.log(userDOB.value);
+    var userInput = userDOB.value.replaceAll('-','');
+    var sum = 0;
+    console.log(luckyNumber.value);
+    for(var i = 0 ; i < userInput.length ; i++) {
+        var digitFromDOB = parseInt(userInput[i]);
+        sum += digitFromDOB;
+    }
+    console.log(sum);
+    if((digitFromDOB%(luckyNumber.value))===0) {
+        outputBox.innerText = "Yay! Your Birthday is Lucky";
+    } else {
+        outputBox.innerText = "Sorry, Your Birthday is not Lucky";
+    }
 }
